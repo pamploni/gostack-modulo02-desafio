7a98f9a13a6c7a6945a513fecce1f096a401a4b3
Pamploni Cadé <pamplonicade@MacBook-Air-de-Pamploni.local>
2020-04-14 19:58:17 -0300

    Desafio modulo02 Front-end com ReactJS feito

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+
+    api.get('repositories')
+    .then(response => {
+      setRepositories(response.data);
+    });
+
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      "title": "Teste Front",
+      "url": "https://www.github.com/pamploni",
+      "techs":["JAVA","JAVASCRIPT","REACT"]
+    });
+
+    setRepositories([...repositories, response.data]);
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+  /*  if (response.status === '204') {
+      const resp = await api.get('repositories');
+      setRepositories([resp.data]);
+    } else {
+      setRepositories(repositories);
+    }
+*/
+    setRepositories();
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories
+          ? repositories.map(item =>
+              <li key= {item.id}>
+                <h1>{item.title}</h1>
+                <button onClick={() => handleRemoveRepository(item.id)}>
+                  Remover
+                </button>
+              </li>)
+          : null
+        }
+        
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={()=>handleAddRepository()}>Adicionar</button>
     </div>
   );
 }
